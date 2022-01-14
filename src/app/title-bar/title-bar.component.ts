bf21fd701526071703fc71661dfce7512cf89abc
Juliane Schmachtenberg <jule-schmachtenberg@web.de>
2022-01-14 14:43:29 +0100

    add database download date

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-title-bar',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title-bar.component.css']
 })
 export class TitleBarComponent implements OnInit {
+  db_date: string;
 
-  constructor() { }
+  constructor(
+      private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    this.get_db_date()
   }
-
+  get_db_date(){
+    this.httpClient.get('assets/db_version.txt', {responseType: 'text'}).subscribe(db_d => this.db_date = db_d)
+  }
 }
